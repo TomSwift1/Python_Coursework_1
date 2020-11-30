8ba703d565ee9328e57e4eb725995ae7c6fcd243
Tom Swift <thomas.swift.20@ucl.ac.uk>
2020-11-30 14:44:21 +0000

    Extra test for too many plot inputs and sex and max_age plotting tools

--- 20146831/test_process_covid.py
+++ 20146831/test_process_covid.py
@@ -1,5 +1,5 @@
 import pytest
-from process_covid import load_covid_data
+from process_covid import load_covid_data,create_confirmed_plot
 from pathlib import Path
 
 def test_data_load():
@@ -7,3 +7,8 @@ def test_data_load():
         data_directory = Path("")
         data_file = "Dodgy_data.json"
         data_er = load_covid_data(data_directory / data_file)
+        
+def test_plot_inputs():
+    with pytest.raises(ValueError):
+        create_confirmed_plot([3,4,5],sex=True,max_ages=[2,3])
+    
